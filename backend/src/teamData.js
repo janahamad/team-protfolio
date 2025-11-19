82bfe71e069a37893e6a10eaf5f4ff3294eaba5f
ranaahmed <rnaahmd240@gmail.com>
2025-11-19 18:46:13 +0300

    teamdata

---
+++ backend/src/teamData.js
@@ -0,0 +1,75 @@
+
+//fixed data for the team using js 
+const teamMembers = [
+  {
+    id: 1,
+    name: "Rana Ahmed",
+    position: "Backend Devloper",
+    image: "",
+    bio: "",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    social: {
+      twitter: "",
+      linkedin: "",
+      github: ""
+    }
+  },
+  {
+    id: 2,
+    name: "Jana",
+    position: "Frontend Developer",
+    image: "",
+    bio: "",
+    skills: [""],
+    social: {
+      twitter: "",
+      linkedin: "",
+      github: ""
+    }
+  },
+  {
+    id: 3,
+    name: "Manar",
+    position: "",
+    image: "",
+    bio: "",
+    skills: [""],
+     social: {
+      twitter: "",
+      linkedin: "",
+      github: ""
+    }
+  },{
+   id: 4,
+    name: "Shatha",
+    position: "",
+    image: "",
+    bio: "",
+    skills: [""],
+     social: {
+      twitter: "",
+      linkedin: "",
+      github: ""
+     }
+    }
+    
+];
+
+const projects = [
+  {
+    id: 1,
+    title: "",
+    description: "",
+    team: [1, 2],
+    technologies: [""]
+  },
+  {
+    id: 2,
+    title: "",
+    description: "",
+    team: [1, 3],
+    technologies: [""]
+  }
+];
+
+module.exports = { teamMembers, projects };
